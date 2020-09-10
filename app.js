bb2adc0da7b421013608b701ea6be964c8598324
zainabalkhdour <zainab.alkhdour@gmail.com>
2020-09-10 21:55:51 +0300

    branch problem4

--- app.js
+++ app.js
@@ -77,12 +77,14 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var sumArr= [2, 3, 4];
+var sumArray=sumArr[0]+sumArr[1]+sumArr[2]
+return[sumArray,`${sumArr[0]},${sumArr[1]},${sumArr[2]} was passed in as an array of numbers, and ${sumArray} is their sum.`];
 }
-
+console.log(sumArray(2,3,4));
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
